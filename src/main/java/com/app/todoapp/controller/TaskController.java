6935a71a2e564b43b1fc7a261fd2266cae2f2a4b
RajjjPrince <ece22144@iiitkalyani.ac.in>
2025-08-11 04:27:31 +0530

    Spring Based ToDo Project

---
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -0,0 +1,42 @@
+package com.app.todoapp.controller;
+
+import com.app.todoapp.model.Task;
+import com.app.todoapp.services.TaskService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+//@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+    @GetMapping
+    public String getTasks(Model model){
+        List<Task> tasks = taskService.getAllTasks();
+        model.addAttribute("tasks",tasks);
+        return"tasks";
+    }
+    @PostMapping
+    public String createTasks(@RequestParam String title){
+        List<Task> tasks = taskService.getAllTasks();
+        taskService.createTask(title);
+        return "redirect:/";
+
+    }
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id){
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id){
+        taskService.toggleTask(id);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/app/todoapp/model/Task.java
@@ -0,0 +1,31 @@
+package com.app.todoapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private boolean completed;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCompleted(boolean b) {
+        this.completed = completed;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+
+}
